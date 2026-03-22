c5c9d23d8867b007dc60f650275aa96efbbfb2ae
Даниил Ликутин <likutindan@gmail.com>
2026-03-23 00:21:40 +0300

    feat(toolbox): expose CommandHistoryStore via public-api

--- packages/toolbox/public-api.ts
+++ packages/toolbox/public-api.ts
@@ -4,5 +4,6 @@ export * from './crypto/service.js';
 export * from './fetch/service.js';
 export * from './gun/service.js';
 export * from './localstorage/service.js';
+export * from './command/history.js';
 export * from './command/command-service.js';
 export * from './command/definition.js';
